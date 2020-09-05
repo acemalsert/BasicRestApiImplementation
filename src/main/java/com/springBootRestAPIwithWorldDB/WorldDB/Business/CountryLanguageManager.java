76e2866dcb913ef3a958d413a55db59c51f96c34
Ahmet Cemal Sert <55139481+acemalsert@users.noreply.github.com>
2020-09-05 18:47:59 +0300

    Update CountryLanguageManager.java

--- src/main/java/com/springBootRestAPIwithWorldDB/WorldDB/Business/CountryLanguageManager.java
+++ src/main/java/com/springBootRestAPIwithWorldDB/WorldDB/Business/CountryLanguageManager.java
@@ -5,11 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-//import com.springBootRestAPIwithWorldDB.WorldDB.DataAccess.ICountryDal;
 import com.springBootRestAPIwithWorldDB.WorldDB.DataAccess.ICountryLanguageDal;
 import com.springBootRestAPIwithWorldDB.WorldDB.Entities.City;
-//import com.springBootRestAPIwithWorldDB.WorldDB.Entities.Country;
 import com.springBootRestAPIwithWorldDB.WorldDB.Entities.CountryLanguage;
 
 @Service
